56f5354521fbea41e8a0aab14ab785204ff6bab3
swastikad912 <swastik.ad912@gmail.com>
2025-04-07 20:00:26 +0530

    Initial commit: Spring Boot backend setup

---
+++ src/main/java/com/splitsync/SplitsyncApplication.java
@@ -0,0 +1,13 @@
+package com.splitsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitsync/SplitsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
