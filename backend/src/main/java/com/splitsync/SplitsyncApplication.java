e2280e468ff368559061ea8547cfb9357ae0e73e
swastikad912 <swastik.ad912@gmail.com>
2025-05-24 21:57:51 +0530

    Updated allowed origins

--- backend/src/main/java/com/splitsync/SplitsyncApplication.java
+++ backend/src/main/java/com/splitsync/SplitsyncApplication.java
@@ -21,7 +21,7 @@ public class SplitsyncApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**")
-						.allowedOrigins("http://localhost:3000") // Your React app
+						.allowedOrigins("http://localhost:3000", "https://splitsync.vercel.app") // Your React app
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
